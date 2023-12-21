31e8b719c1340b913ce5659f978e735c091e47b7
Runa <108367143+RunaNay@users.noreply.github.com>
2023-12-21 19:05:05 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+alert('Hello world');
+
+//show my fav food in the index
+let favouriteFood = 'Raspberry';
+document.write(favouriteFood);
+
+//show my least-liked food in the index too
+let leastLikedfood = 'Apple';
+document.write(leastLikedfood);
