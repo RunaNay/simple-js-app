9d25ab05cfbb4ebf331a5d4a2f0f63710e19f552
Runa <108367143+RunaNay@users.noreply.github.com>
2024-01-22 21:14:56 +0100

    create PokeList
    cretae variables and arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,72 @@
-alert('Hello world');
+pokemonList = [
 
-//show my fav food in the index
-let favouriteFood = 'Raspberry';
-document.write(favouriteFood);
+    {
+        name: 'Pikachu',
+        height: 0.4,
+        type: 'electric',
+        abilities: ['static', 'lightningrod'] }
 
-//show my least-liked food in the index too
-let leastLikedfood = 'Apple';
-document.write(leastLikedfood);
 
+        {
+            name: 'Bulbasor',
+            height: 2.04,
+            type: 'seed',
+            abilities: ['overgrow'] }
+
+
+        {
+            name: 'Wartortle',
+            height: 3.03,
+            type: 'turtle',
+            abilities: ['torrent'] }
+
+
+{
+    name: 'Vulpix',
+    height: 2.00,
+    type: 'fox',
+    abilities: ['flash fire']}
+
+    {
+        name: 'Metapod',
+        height: 2.04,
+        type: 'cocoon',
+        abilities: ['shed skin']}
+
+        {
+            name: 'Kakuna',
+            height: 2.00,
+            type: 'cocoon',
+            abilities: ['shed skin']}
+
+
+            {
+                name: 'Zubat',
+                height: 2.07,
+                type: 'bat',
+                abilities: ['inner focus']}
+
+
+                {
+                    name: 'Beedrill',
+                    height: 3.03,
+                    type: 'poisonbee',
+                    abilities: ['swarm']}
+
+
+                    {
+                        name: 'Nidorina',
+                        height:2.07,
+                        type: 'poison pin',
+                        abilities: ['poison point, rivalry']}
+
+
+                        {
+                            name: 'Venomoth',
+                            height: 4.11,
+                            type: 'poison moth',
+                            abilities: [shield dust, tinted lens]}
+
+
+
+        ]
