ac9eb64c3eb50900b4cae90bef851091e0abfc26
Runa <108367143+RunaNay@users.noreply.github.com>
2024-01-24 20:00:48 +0100

    edit code
    formatted

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,81 @@
-pokemonList = [
+.
 
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        type: 'electric',
-        abilities: ['static', 'lightningrod'] }
+const pokemonList =
+
+    [{
+            name: 'Pikachu',
+            height: 0.4,
+            type: 'electric',
+            abilities: ['static', 'lightningrod']
+        },
 
 
         {
             name: 'Bulbasor',
             height: 2.04,
             type: 'seed',
-            abilities: ['overgrow'] }
+            abilities: ['overgrow']
+        },
 
 
         {
             name: 'Wartortle',
             height: 3.03,
             type: 'turtle',
-            abilities: ['torrent'] }
+            abilities: ['torrent']
+        },
 
 
-{
-    name: 'Vulpix',
-    height: 2.00,
-    type: 'fox',
-    abilities: ['flash fire']}
+        {
+            name: 'Vulpix',
+            height: 2.00,
+            type: 'fox',
+            abilities: ['flash fire']
+        },
 
-    {
-        name: 'Metapod',
-        height: 2.04,
-        type: 'cocoon',
-        abilities: ['shed skin']}
+        {
+            name: 'Metapod',
+            height: 2.04,
+            type: 'cocoon',
+            abilities: ['shed skin']
+        },
 
         {
             name: 'Kakuna',
             height: 2.00,
             type: 'cocoon',
-            abilities: ['shed skin']}
+            abilities: ['shed skin']
+        },
 
 
-            {
-                name: 'Zubat',
-                height: 2.07,
-                type: 'bat',
-                abilities: ['inner focus']}
+        {
+            name: 'Zubat',
+            height: 2.07,
+            type: 'bat',
+            abilities: ['inner focus']
+        },
 
 
-                {
-                    name: 'Beedrill',
-                    height: 3.03,
-                    type: 'poisonbee',
-                    abilities: ['swarm']}
+        {
+            name: 'Beedrill',
+            height: 3.03,
+            type: 'poisonbee',
+            abilities: ['swarm']
+        },
 
 
-                    {
-                        name: 'Nidorina',
-                        height:2.07,
-                        type: 'poison pin',
-                        abilities: ['poison point, rivalry']}
+        {
+            name: 'Nidorina',
+            height: 2.07,
+            type: 'poison pin',
+            abilities: ['poison point, rivalry']
+        },
 
 
-                        {
-                            name: 'Venomoth',
-                            height: 4.11,
-                            type: 'poison moth',
-                            abilities: [shield dust, tinted lens]}
-
-
-
-        ]
+        {
+            name: 'Venomoth',
+            height: 4.11,
+            type: 'poison moth',
+            abilities: [shield dust, tinted lens]
+        }
+    ]
