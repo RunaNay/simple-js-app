ec6c71848e2d5092c4f37cbcd83c5928c7b80988
Runa <108367143+RunaNay@users.noreply.github.com>
2024-03-19 00:38:32 +0100

    Update scripts.js
    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
+var pokemonRepository = (function() {
+    var pokemonList = [
+        { name: 'Pikachu', height: 0.4 },
+        { name: 'Bulbasor', height: 2.04 },
+        { name: 'Wartortle', height: 3.03 },
+        { name: 'Vulpix', height: 2.00 },
+        { name: 'Metapod', height: 2.04 },
+        { name: 'Kakuna', height: 2.00 },
+        { name: 'Zubat', height: 2.07 },
+        { name: 'Beedrill', height: 3.03 },
+        { name: 'Nidorina', height: 2.07 },
+        { name: 'Venomoth', height: 4.11 }
+    ];
 
-let pokemonList = [
-    { name: 'Pikachu', height: 0.4 },
-    { name: 'Bulbasor', height: 2.04 },
-    { name: 'Wartortle', height: 3.03 },
-    { name: 'Vulpix', height: 2.00 },
-    { name: 'Metapod', height: 2.04 },
-    { name: 'Kakuna', height: 2.00 },
-    { name: 'Zubat', height: 2.07 },
-    { name: 'Beedrill', height: 3.03 },
-    { name: 'Nidorina', height: 2.07 },
-    { name: 'Venomoth', height: 4.11 }
-]
+    return {
+        getAll: function() {
+            return pokemonList;
+        },
+        add: function(item) {
+            pokemonList.push(item);
+        }
+    };
+})();
 
-
-
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 4) {
         document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ') Wow, das ist riesig!</p>');
     } else {
         document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ')</p>');
     }
 });
-
-
-
